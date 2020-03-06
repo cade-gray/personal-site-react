5853e1ce79aa4e04b7b2a2f3ecc5733adbba8e3e
Cade Gray <cgray35@radford.edu>
2020-03-06 01:56:26 -0500

    Changed font once again

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -9,9 +9,8 @@ const resume = () => {
     return(
         <div className = 'resume-container'>
             <div>
-            <h4><a href={MyResumePDF} download>Download this resume for a better viewing experience!</a></h4>
                 <img src={MyResumeImg} className='resume-img' alt='My resume' />
-                <br />
+                <h4><a href={MyResumePDF} download>Download </a></h4>
            </div>
         </div>
     )
