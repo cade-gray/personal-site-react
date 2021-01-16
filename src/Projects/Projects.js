e9de88e6057229360d5c8edb75100f87f155c29f
Cade Gray <cadegrayweb@gmail.com>
2021-01-16 17:23:28 -0500

    Added false values for project components

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -20,10 +20,10 @@ const projects = () => {
                 <Project imgURL={[itecMajorPic]} projName="Radford University ITEC Major Viewer" githubLink="https://github.com/cade-gray/itec-majors-viewer"
                 projDesc="For my Advanced Web Development class, we were required to construct a javascript application that showcased Radford University's Information Technology Majors and their corresponding concentrations.  The user could then view 3 classes that are specific to the selected concentraton. This application implemented HTML, CSS, JavaScript, and jQuery."
                 />
-                <Project className='project-comp' imgURL={[vaultairePic]} projName="The Vault" 
+                <Project className='project-comp' imgURL={[vaultairePic]} projName="The Vault" githubLink={false}
                 projDesc="In my Web Development II class, I was assigned to a group to create a website that implemented the LAMP stack as well as form validation and sanitization. The website would have a user create an account and they could then upload files to be shared with other users by creating groups and adding them to the groups. I worked with the front end using HTML, CSS, JavaScript, and Bootstrap CSS. I also worked some with the back end using PHP and SQL."
                 />
-                <Project imgURL={[MHHPic1,MHHPic2]} projName="Muse Hall Hop"
+                <Project imgURL={[MHHPic1,MHHPic2]} projName="Muse Hall Hop" githubLink={false}
                 projDesc="For my Computer Science II class, I was assigned to create a game using a library of my choice. I chose to use JavaFX and created a game that used the same mechanics as the mobile game flappy bird but gave it a theme that referred to my school's dorm, Muse Hall."
                 />
             </div>
