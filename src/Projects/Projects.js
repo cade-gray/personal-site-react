aa4e376c41a487a4ccfbf0605550b1b7452ea570
Cade Gray <cadegrayweb@gmail.com>
2021-03-12 20:34:24 -0500

    Removed muse hall hop from projects page

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Projects.css';
 import vaultairePic from '../images/vaultaire.jpg';
-import MHHPic1 from '../images/titleScreen.png';
-import MHHPic2 from '../images/charScreen.png';
 import itecMajorPic from '../images/itecMajorViewer.png'
 import colorPal1 from '../images/colorPalHome.png'
 import colorPal2 from '../images/colorPalCreate.png'
@@ -21,9 +19,6 @@ const projects = () => {
                 />
                 <Project className='project-comp' imgURL={[vaultairePic]} projName="The Vault" githubLink={false}
                 projDesc="In my Web Development II class, I was assigned to a group to create a website that implemented the LAMP stack as well as form validation and sanitization. The website would have a user create an account and they could then upload files to be shared with other users by creating groups and adding them to the groups. I worked with the front end using HTML, CSS, JavaScript, and Bootstrap CSS. I also worked some with the back end using PHP and SQL."
-                />
-                <Project imgURL={[MHHPic1,MHHPic2]} projName="Muse Hall Hop" githubLink={false}
-                projDesc="For my Computer Science II class, I was assigned to create a game using a library of my choice. I chose to use JavaFX and created a game that used the same mechanics as the mobile game flappy bird but gave it a theme that referred to my school's dorm, Muse Hall."
                 />
             </div>
         </div>
