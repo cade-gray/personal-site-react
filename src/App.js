71fd8ff9e67d69231689ea975a083dcd0d138262
Cade Gray <cadegrayweb@gmail.com>
2021-03-12 20:32:26 -0500

    Added footer to site

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Logo from './Logo/Logo';
 import Home from './Home/Home';
 import Projects from './Projects/Projects'
 import ScrollToTop from './ScrollToTop';
+import Footer from './Footer/Footer';
 function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ function App() {
           <Logo />
           <Route exact path='/' component={Home} />
           <Route path='/projects' component={Projects} />
+          <Footer />
       </div>
     </Router>
   );
