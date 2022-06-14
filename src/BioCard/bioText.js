10c444ee98be0e851fd70154e83091e507d85857
Cade Gray <cadegrayweb@gmail.com>
2022-06-13 22:59:57 -0400

    Fixed spelling error and changed text again

--- src/BioCard/bioText.js
+++ src/BioCard/bioText.js
@@ -2,5 +2,5 @@ var dob = new Date("06/06/1998");
 var ageDT = new Date(Date.now() - dob.getTime());
 var age = Math.abs(ageDT.getUTCFullYear() - 1970);
 module.exports = {
-  bioText: `${age} year old from East Tennessee with experience in multiple facets of the I.T field.  Graduated from Radford University with Bachelor of Science degree in Information Science and Systems with a concentration in Web Development. Skills include but are not limited to: Fullstack Web Development with JavaScript and Computer Science Fundamnetals with Java, SQL and Database management with SQL Server and Oracle, Microsoft SCCM, and application of both computer science and information technology concepts in a financial industry context.`,
+  bioText: `${age} year old from East Tennessee with experience in multiple facets of the I.T field.  Graduated from Radford University with Bachelor of Science degree in Information Science and Systems with a concentration in Web Development. Skills include but are not limited to: Fullstack Web Development with JavaScript and Computer Science fundamentals with Java, SQL and database management with SQL Server and Oracle, Microsoft SCCM, and application of these concepts in a financial industry context.`,
 };
