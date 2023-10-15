733837530cd7f4fc857e2e7fe36b94a8c8080a5a
Cade Gray <cadegrayweb@gmail.com>
2023-10-15 13:54:27 -0400

    Update biotext and move relevant info to About.js

--- src/components/BioCard/bioText.js
+++ src/components/BioCard/bioText.js
@@ -1,6 +1,3 @@
-var dob = new Date("06/06/1998");
-var ageDT = new Date(Date.now() - dob.getTime());
-var age = Math.abs(ageDT.getUTCFullYear() - 1970);
 module.exports = {
-  bioText: `${age} year old from East Tennessee with experience in multiple facets of the I.T field.  Graduated from Radford University with Bachelor of Science degree in Information Science and Systems with a concentration in Web Development. Skills include but are not limited to: Fullstack Web Development with JavaScript and Computer Science fundamentals with Java, SQL and database management with SQL Server and Oracle, Microsoft SCCM, and application of these concepts in a financial industry context.`,
+  bioText: `Creating efficient software solutions that will empower businesses and users to reach their full potential and stay ahead in the modern age of technology.`,
 };
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,12 +6,17 @@ const About = () => {
       <h2>About Me</h2>
       <h3>What do I do for work?</h3>
       <p>
-        I work full time as a Systems Analyst for Tennesse's top Credit Union. I
-        also work on the side as a web developer, creating websites or software
-        solutions for businesses, especially smaller ones in my area of
-        Northeast Tennessee.
+        I work full time as a Systems Analyst for Tennesse's top Credit Union,
+        supporting critical finanical applications and creating software
+        solutions to grow the business, improve staff workflow, and empower them
+        to make data driven decisions.
       </p>
-      <h3>What languages/frameworks have I developed with?</h3>
+      <p>
+        I also work on the side as a software developer, focusing mostly in the
+        web realm, creating websites, web services, and software solutions for
+        businesses and individuals.
+      </p>
+      <h3>What languages/frameworks/tools have I developed with?</h3>
       <ul>
         <li>Javascript / TypeScript / Node</li>
         <ul>
@@ -19,6 +24,17 @@ const About = () => {
           <li>Svelte / SvelteKit</li>
           <li>Express for API Development</li>
           <li>SailsJS</li>
+        </ul>
+        <li>SQL</li>
+        <ul>
+          <li>Oracle</li>
+          <ul>
+            <li>ODBC Connectivity</li>
+          </ul>
+          <li>SQL Server</li>
+          <ul>
+            <li>SQL Server Integration Services</li>
+          </ul>
         </ul>
         <li>Go</li>
         <li>Java</li>
