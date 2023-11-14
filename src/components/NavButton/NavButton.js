d2feeca136ace5e7da7c645e28669e28c2bd51c7
Cade Gray <47758856+cade-gray@users.noreply.github.com>
2023-11-13 22:32:00 -0500

    Updated nav button and added blog link

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -2,11 +2,21 @@ import React from "react";
 import "./NavButton.css";
 import { Link } from "react-router-dom";
 export default function NavButton(props) {
-  return (
-    <div className="navButton">
-      <Link to={props.link}>
-        <span className="linkText">{props.label}</span>
-      </Link>
-    </div>
-  );
+  if (!props.ext) {
+    return (
+      <div className="navButton">
+        <Link to={props.link}>
+          <span className="linkText">{props.label}</span>
+        </Link>
+      </div>
+    );
+  } else {
+    return (
+      <div className="navButton">
+        <a href={props.link}>
+          <span className="linkText">{props.label}</span>
+        </a>
+      </div>
+    );
+  }
 }
