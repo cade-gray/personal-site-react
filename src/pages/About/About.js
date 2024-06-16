2830d8324721edb617ac212a263e1ad481669f74
Cade Gray <47758856+cade-gray@users.noreply.github.com>
2024-06-16 13:06:27 -0400

    Updated about me page with current info

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,17 +4,17 @@ const About = () => {
   return (
     <div className="aboutContainer">
       <h2>About Me</h2>
-      <h3>What do I do for work?</h3>
+      <h3>What do I do for a living?</h3>
       <p>
-        I work full time as a Systems Analyst for Tennesse's top Credit Union,
-        supporting critical finanical applications and creating software
-        solutions to grow the business, improve staff workflow, and empower them
-        to make data driven decisions.
+        I work full time as a Systems Analyst for one of the United State's top
+        Credit Unions, supporting critical finanical applications and creating
+        software solutions to grow the business, improve staff workflows, and
+        empower them to make data driven decisions.
       </p>
       <p>
-        I also work on the side as a software developer, focusing mostly in the
-        web realm, creating websites, web services, and software solutions for
-        businesses and individuals.
+        I also work on the side, more as a hobby curently, as a software
+        developer. I focus mostly in the web realm, creating web apps, web
+        services, and software solutions for myself and others.
       </p>
       <h3>What languages/frameworks/tools have I developed with?</h3>
       <ul>
@@ -23,7 +23,11 @@ const About = () => {
           <li>React</li>
           <li>Svelte / SvelteKit</li>
           <li>Express for API Development</li>
-          <li>SailsJS</li>
+          <li>Building RESTful API's</li>
+          <li>PM2</li>
+          <li>
+            Deployment of these technologies in Linux and Windows environments.
+          </li>
         </ul>
         <li>SQL</li>
         <ul>
@@ -33,11 +37,31 @@ const About = () => {
           </ul>
           <li>SQL Server</li>
           <ul>
-            <li>SQL Server Integration Services</li>
+            <li>SQL Server Integration Services (SSIS)</li>
           </ul>
+          <li>MySQL</li>
         </ul>
         <li>Go</li>
+        <ul>
+          <li>Building services that intereact with API's</li>
+        </ul>
+        <li>PowerShell</li>
         <li>Java</li>
+        <li>Docker</li>
+      </ul>
+      <h3>What other skills have I picked up from my career?</h3>
+      <ul>
+        <li>Applying technology in a finanical context</li>
+        <li>
+          Financial Software knowledge (Fiserv, MeridianLink, Jack Henry, etc..)
+        </li>
+        <li>Project Management</li>
+        <li>Business Analysis</li>
+        <li>Process Improvement</li>
+        <li>Training</li>
+        <li>Public Speaking</li>
+        <li>Team Leadership</li>
+        <li>Customer Service</li>
       </ul>
     </div>
   );
